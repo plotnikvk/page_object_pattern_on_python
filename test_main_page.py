9a916aebc2e49843874484ccd2856e1244f446c5
Plotnikov Vladimir <plotnikvk@gmail.com>
2019-11-16 12:28:22 +0300

    Add page_object files in project

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,7 @@
-def go_to_login_page(browser):
-    link = browser.find_element_by_css_selector("#login_link")
-    link.click()
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
